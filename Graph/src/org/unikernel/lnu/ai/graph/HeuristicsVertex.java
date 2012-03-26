681730e75c4553c7492638f75de8589b6bb52523
anygizer <mcangel@unikernel.net>
2012-03-27 02:06:01 +0300

    Added IDA* algorithm and necessary stuff to the graph.
    Next points:
     - unit-tests for the IDA*
     - visualization stuff for the algorithm
     - everything else

---
+++ Graph/src/org/unikernel/lnu/ai/graph/HeuristicsVertex.java
@@ -0,0 +1,53 @@
+package org.unikernel.lnu.ai.graph;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author mcangel
+ */
+public class HeuristicsVertex extends Vertex
+{
+	private int heuristics;
+
+	public HeuristicsVertex(int heuristics, String label, Point preferredLocation)
+	{
+		super(label, preferredLocation);
+		this.heuristics = heuristics;
+	}
+
+	public HeuristicsVertex(int heuristics, String label)
+	{
+		super(label);
+		this.heuristics = heuristics;
+	}
+
+	public HeuristicsVertex(int heuristics)
+	{
+		this.heuristics = heuristics;
+	}
+	
+	public HeuristicsVertex(String label, Point preferredLocation)
+	{
+		super(label, preferredLocation);
+	}
+
+	public HeuristicsVertex(String label)
+	{
+		super(label);
+	}
+
+	public HeuristicsVertex()
+	{
+	}
+
+	public int getHeuristics()
+	{
+		return heuristics;
+	}
+
+	public void setHeuristics(int heuristics)
+	{
+		this.heuristics = heuristics;
+	}
+}
